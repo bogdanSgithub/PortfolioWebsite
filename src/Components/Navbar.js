f3b4b180efdad4bfc8e68cd5ab3eec34822a5344
Bogdan Feher <bogdan.feher@gmail.com>
2024-09-01 16:18:58 -0400

    added icons to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,3 +1,10 @@
+import {
+  BsFillHouseFill,
+  BsCodeSlash,
+  BsFileTextFill,
+  BsFillTelephoneFill,
+} from "react-icons/bs";
+
 function Navbar() {
   return (
     <nav className="flex items-center justify-between flex-wrap">
@@ -12,10 +19,22 @@ function Navbar() {
           <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
         </svg>
       </div>
-      <a href="#About">About</a>
-      <a href="#Projects">Projects</a>
-      <a href="#Experience">Experience</a>
-      <a href="#Contact">Contact</a>
+      <div className="flex flex-row">
+        <BsFillHouseFill className="fill-primary" />
+        <a href="#About">About</a>
+      </div>
+      <div className="flex flex-row">
+        <BsCodeSlash className="fill-primary" />
+        <a href="#Projects">Projects</a>
+      </div>
+      <div className="flex flex-row">
+        <BsFileTextFill className="fill-primary" />
+        <a href="#Experience">Experience</a>
+      </div>
+      <div className="flex flex-row">
+        <BsFillTelephoneFill className="fill-primary" />
+        <a href="#Contact">Contact</a>
+      </div>
     </nav>
   );
 }
