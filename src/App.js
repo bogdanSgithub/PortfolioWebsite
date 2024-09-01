ebf6178c6291a4a653cb9b6d2a853a33b86e917e
Bogdan Feher <bogdan.feher@gmail.com>
2024-09-01 15:09:39 -0400

    added About component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import About from "./Components/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h1>About me</h1>
+      <p>bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+      <p>image will be here</p>
+    </div>
+  );
+}
+
+export default About;
