7c08132abc50015527d00a5b091bfd0b108f6d2f
Bogdan Feher <bogdan.feher@gmail.com>
2024-09-01 15:44:57 -0400

    added tailwind to app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#1b1a17",
+        primary: "#c78e35",
+        firstText: "#d8dad5",
+        secondText: "#b3b5b0",
+      },
+    },
+  },
+  plugins: [],
+};
