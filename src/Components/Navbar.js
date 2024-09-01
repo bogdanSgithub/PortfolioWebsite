fe95fccde46996252e69ec568c0d37347fc5bdd2
Bogdan Feher <bogdan.feher@gmail.com>
2024-09-01 15:01:27 -0400

    added navbar component

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <div className="App">
+      <p>LOGO</p>
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Experience</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
