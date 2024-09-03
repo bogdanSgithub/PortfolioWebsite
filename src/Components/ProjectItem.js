d8604d1116513749cb723e73126f5e2e8faf0b43
Bogdan Feher <bogdan.feher@gmail.com>
2024-09-03 16:00:46 -0400

    added portfolio to project section aswell as image for jachacks

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -5,19 +5,26 @@ import { FiExternalLink } from "react-icons/fi";
 function ProjectItem({ project }) {
   return (
     <section className="flex flex-col md:flex-row items-center">
-      <aside className="max-md:hidden flex-shrink-0 md:w-1/2">
-        <img
-          src={project.img}
-          alt={`${project.title}`}
-          className="w-full h-auto rounded-lg"
-        />
+      <aside className="max-md:hidden flex-shrink-0 md:w-1/2 mt-8 mb-8">
+        <div className="bg-primary opacity-70 transition-opacity duration-300 ease-in-out hover:opacity-100 w-[90%] rounded-[12px] ">
+          <img
+            src={project.img}
+            alt={`${project.title}`}
+            className="rounded-[12px] "
+          />
+        </div>
       </aside>
       <article
         className="max-md:bg-secondBackground md:w-1/2 md:text-right max-md:p-12 mt-6 rounded bg-gradient-to-br max-md:from-secondBackground 
                 to-background"
       >
         <h2 className="text-3xl text-primary mb-4">{project.title}</h2>
-        <p className="mb-3 text-secondText">{project.description}</p>
+        <p
+          className="mb-3 text-secondText min-md: bg-secondBackground rounded bg-gradient-to-br min-md:from-secondBackground 
+                to-background p-4"
+        >
+          {project.description}
+        </p>
         <ul className="mb-3 text-secondText">
           {project.tools.map((tool, index) => (
             <li
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,12 +1,13 @@
 import { BsCodeSlash } from "react-icons/bs";
 import ProjectItem from "./ProjectItem";
+import jacHacksPic from "../pictures/jachacks.png";
 
 const projects = [
   {
     title: "MarketPath",
     description:
       "Project developed for AI Launch Lab's R&D program. MarketPath is an AI-powered machine learning model for investment analysis.",
-    img: "path/to/ai-web-app-image.jpg", // Replace with the actual path or URL to your image
+    img: "",
     tools: ["Streamlit", "Scikit Learn", "Python"],
     githubLink: "https://github.com/bogdanSgithub/MarketPath",
     websiteLink: "https://marketpath.streamlit.app/",
@@ -14,10 +15,19 @@ const projects = [
   {
     title: "JACHacks Website",
     description:
-      "The official website for JACHacks, established in march 2024. Designed and implemented by students of John Abbott College.",
-    img: "path/to/fintech-dashboard-image.jpg", // Replace with the actual path or URL to your image
+      "The official website for JACHacks, established in march 2024. Designed and coded by students of John Abbott College.",
+    img: jacHacksPic, // Replace with the actual path or URL to your image
     tools: ["JavaScript", "HTML", "CSS"],
     githubLink: "https://github.com/JACHacks/JACHacks-Website",
+    websiteLink: "https://jachacks.pages.dev/",
+  },
+  {
+    title: "Porfolio",
+    description:
+      "Built in React and TailwindCSS, this personal portfolio features my top projects, experiences, technical skills, and a brief introduction about me.",
+    img: "path/to/fintech-dashboard-image.jpg", // Replace with the actual path or URL to your image
+    tools: ["React", "JavaScript", "TailwindCSS"],
+    githubLink: "https://github.com/bogdanSgithub/bogdanSgit",
     websiteLink: "https://jachacks.pages.dev/",
   },
 ];
