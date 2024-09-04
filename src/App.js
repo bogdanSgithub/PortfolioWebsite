944f5c02eb554ff88fe6f75eb02e7148119ac5d3
Bogdan <bogdan.feher@gmail.com>
2024-09-04 17:57:43 -0400

    fixed web app title at top

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
@@ -8,6 +10,9 @@ import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 
 function App() {
+  useEffect(() => {
+    document.title = "Bogdan Feher"
+ }, []);
   return (
     <div className="App bg-background text-firstText font-sans">
       <Navbar />
