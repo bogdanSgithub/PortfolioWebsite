18ae9d544f18287127b69c624f17ee8571cf76d3
Bogdan Feher <bogdan.feher@gmail.com>
2024-09-04 20:38:31 -0400

    made nav font bigger for small devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
     extend: {
       colors: {
         background: "#1b1a17",
